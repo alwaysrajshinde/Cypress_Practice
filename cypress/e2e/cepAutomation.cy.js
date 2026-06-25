4a691d2fbe7df11ecdc36ccdc1d75916b4d98f91
Raj Shinde <alwaysrajshinde@gmail.com>
2026-06-26 00:07:19 +0530

    added function for login and logout

--- cypress/e2e/cepAutomation.cy.js
+++ cypress/e2e/cepAutomation.cy.js
@@ -1,6 +1,6 @@
 describe("Automations", () => {
 
-    it("Flow 1: Login → Open Class → Open Topic → Logout", () => {
+    function login() {
         cy.visit("https://ce-qa-school.devstudi.com/teach/whiteboard")
         cy.get(".login-modal-outer").click()
 
@@ -8,7 +8,15 @@ describe("Automations", () => {
         for (let i = 0; i < otp.length; i++) {
             cy.get(`#otp-input-${i}`).type(`${otp[i]}`)
         }
+    }
 
+    function signout () {
+        //signout
+        cy.xpath("//span[@class='userimg']").click()
+        cy.xpath("//div[@class='mat-body-1']").click()
+    }
+    it("Flow 1: Login → Open Class → Open Topic → Logout", () => {
+        login()
         cy.get('[data-qa="playlist-current-grade-subject-btn"] > span').click()
         cy.xpath("//span[contains(text(),'All My Classes')]").click()
         cy.xpath("//span[normalize-space()='Class 12']").click()
@@ -21,34 +29,23 @@ describe("Automations", () => {
         cy.get('.close-icon.ng-star-inserted').click()
 
         //signout
-        cy.xpath("//span[@class='userimg']").click()
-        cy.xpath("//div[@class='mat-body-1']").click()
+        signout()
 
         cy.get(".login-modal-outer").should("be.visible")
 
 
     })
     it("Flow 2: Login → Recent Classes", () => {
-        cy.visit("https://ce-qa-school.devstudi.com/teach/whiteboard")
-        cy.get(".login-modal-outer").click()
-
-        let otp = "90005";
-        for (let i = 0; i < otp.length; i++) {
-            cy.get(`#otp-input-${i}`).type(`${otp[i]}`)
-        }
+        login()
 
         cy.get('[data-qa="playlist-current-grade-subject-btn"] > span').click()
 
         //select 2nd recent class 
         cy.get(':nth-child(2) > .mdc-button__label > :nth-child(2) > .class-name').click()
 
-        //signout
-        cy.xpath("//span[@class='userimg']").click()
-        cy.xpath("//div[@class='mat-body-1']").click()
-
+        signout()
         cy.get(".login-modal-outer").should("be.visible")
-
-
+        
 
     })
 
